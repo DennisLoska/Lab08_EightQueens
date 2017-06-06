16417a8bc66e860a726202fea6feaec877c75c5c
DennisTalmid <d.loska@hotmail.de>
2017-06-06 14:48:38 +0200

    adding a method for creating the int-chessboard

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,5 +1,31 @@
 public class Chessboard {
+
+    public int[][] chessBoard = new int[8][8];
+
+    public Chessboard(){
+        createBoard(chessBoard);
+    }
+
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Chessboard board = new Chessboard();
+    }
+
+    /*
+        Erstellt ein Schachbrett aus ints, wo die 0 für "weiß" und 1 für "schwarz steht. Dieses wird dann in der Konsole angezeigt.
+     */
+    private void createBoard(int[][] chessBoard){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i%2==0) {
+                    if (j % 2 == 0) chessBoard[i][j] = 0;
+                    else chessBoard[i][j] = 1;
+                } else {
+                    if (j % 2 != 0) chessBoard[i][j] = 0;
+                    else chessBoard[i][j] = 1;
+                }
+                System.out.print(chessBoard[i][j]+" ");
+                if (j==7) System.out.print("\n");
+            }
+        }
     }
 }
